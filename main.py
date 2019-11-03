72945497a66824cb8c67799393076b263938f07e
geonerus <geonerus@localhost.localdomain>
2019-11-03 16:51:11 +0300

    2

--- main.py
+++ main.py
@@ -1,10 +1,26 @@
-from flask import Flask, request
-app = Flask(__name__)
+from aiohttp import web
+from aioalice import Dispatcher, get_new_configured_app, types
+from aioalice.dispatcher import MemoryStorage
 
-@app.route('/',methods=["POST"])
-def hello_world():
-    print(request.json)
-    return 'Hello World!'
+
+WEBHOOK_URL_PATH = ''  # webhook endpoint
+WEBAPP_HOST = 'localhost'
+WEBAPP_PORT = 8081
+dp = Dispatcher(storage=MemoryStorage())
+
+@dp.request_handler(func=lambda areq: areq.session.new)
+async def handle_new_session(alice_request):
+    return alice_request.response('Привет! Этот навык позволяет вычислить наминал  резистора по цветовой маркеровке. Для выхода из навыка скажите клиса хватиттр. Сколько полос на резисторе? Четыре или пять?')
+
+
+async def handle_all_other_requests(alice_request):
+    # Всеми силами убеждаем пользователя купить слона,
+    # предлагаем варианты ответа на основе текста запроса
+    requst_text = alice_request.request.original_utterance
+    suggests = await get_suggests(alice_request.session.user_id)
+    return alice_request.response('Все говорят "цвет или количество колец названы неверно. скажите справка для списка команд')
+
 
 if __name__ == '__main__':
-    app.run()
+    app = get_new_configured_app(dispatcher=dp, path=WEBHOOK_URL_PATH)
+    web.run_app(app, host=WEBAPP_HOST, port=WEBAPP_PORT)
